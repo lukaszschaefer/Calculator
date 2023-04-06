a375d4fc6136cc99a8541688771b3753d2e9fb21
lukaszschaefer <112589103+lukaszschaefer@users.noreply.github.com>
2023-04-06 10:24:47 +0200

    Multiplication done

--- calculator-app/src/01_components/MultiplicationComponent/MultiplicationComponent.tsx
+++ calculator-app/src/01_components/MultiplicationComponent/MultiplicationComponent.tsx
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 const MultiplicationComponent: React.FC = () => {
+  const [number1, setNumber1] = useState<number>(0);
+  const [number2, setNumber2] = useState<number>(0);
+  const [result, setResult] = useState<number>(0);
+
+  const handleNumber1Change = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setNumber1(Number(event.target.value));
+  };
+
+  const handleNumber2Change = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setNumber2(Number(event.target.value));
+  };
+
+  const multiplyNumbers = () => {
+    const result = number1 * number2;
+    setResult(result);
+  };
+
   return (
     <>
       <h1>Multiplication</h1>
-      <input type="number" />
-      <input type="number" />
-      <button>Enter</button>
+      <input type="number" value={number1} onChange={handleNumber1Change} />
+      <input type="number" value={number2} onChange={handleNumber2Change} />
+      <button onClick={multiplyNumbers}>Enter</button>
+      <h1>{result}</h1>
     </>
   );
 };
